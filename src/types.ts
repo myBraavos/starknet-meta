7c8eec2e63ea9079e544b9c3da1244e623409747
Abraham Makovetsky <avimak@gmail.com>
2023-04-05 12:24:49 +0300

    v0.0.4

--- src/types.ts
+++ src/types.ts
@@ -1,34 +1,27 @@
 export interface ProjectMetadata {
-    id: string;
-    displayName: string;
-    description: string;
-    host:
-        | string
-        | {
-              "mainnet-alpha"?: string;
-              "goerli-alpha"?: string;
-              "goerli-alpha-2"?: string;
-              others?: string;
-          };
-    contracts: {
-        tag: string;
-        implements?: ("erc721" | "erc20" | "erc1155")[];
-        addresses: {
-            "mainnet-alpha"?: string[];
-            "goerli-alpha"?: string[];
-            "goerli-alpha-2"?: string[];
-        };
-        [k: string]: unknown;
-    }[];
-    categories: (
-        | "nft"
-        | "defi"
-        | "mobile"
-        | "infra"
-        | "gamefi"
-        | "digitalid"
-    )[];
+  id: string;
+  displayName: string;
+  description: string;
+  host:
+    | string
+    | {
+        "mainnet-alpha"?: string;
+        "goerli-alpha"?: string;
+        "goerli-alpha-2"?: string;
+        others?: string;
+      };
+  contracts: {
+    tag: string;
+    implements?: ("erc721" | "erc20" | "erc1155")[];
+    addresses: {
+      "mainnet-alpha"?: string[];
+      "goerli-alpha"?: string[];
+      "goerli-alpha-2"?: string[];
+    };
     [k: string]: unknown;
+  }[];
+  categories: ("nft" | "defi" | "mobile" | "infra" | "gamefi" | "digitalid")[];
+  [k: string]: unknown;
 }
 
 export interface Project {
