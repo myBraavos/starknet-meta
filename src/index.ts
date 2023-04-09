dcb080439f7ee8a70751312e6aef8c8ffe756622
Abraham Makovetsky <avimak@gmail.com>
2023-04-09 17:27:41 +0300

    chore: export Category

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Project } from "./types";
+import type { Category, Project } from "./types";
 import { contractsMap, normalizeAddress, projectsMap } from "./parser";
 
 export const list = (): Project[] => Object.values(projectsMap);
@@ -8,4 +8,4 @@ export const get = (id: string): Project | undefined => projectsMap[id];
 export const getByContract = (address: string): Project | undefined =>
     contractsMap[normalizeAddress(address)];
 
-export { Project };
+export { Category, Project };
