facc77b51782cd9d76d5f0c533eb480819f5d118
Abraham Makovetsky <avimak@gmail.com>
2023-05-10 01:12:57 +0300

    fix types prettier

--- src/types.ts
+++ src/types.ts
@@ -1,38 +1,38 @@
 export type Category =
-  | "nft"
-  | "defi"
-  | "mobile"
-  | "infra"
-  | "gamefi"
-  | "digitalid";
+    | "nft"
+    | "defi"
+    | "mobile"
+    | "infra"
+    | "gamefi"
+    | "digitalid";
 
 export interface ProjectMetadata {
-  id: string;
-  displayName: string;
-  description: string;
-  host:
-    | string
-    | {
-        "mainnet-alpha"?: string,
-        "goerli-alpha"?: string,
-        "goerli-alpha-2"?: string,
-        others?: string,
-      };
-  contracts: {
-    tag: string,
-    implements?: ("erc721" | "erc20" | "erc1155")[],
-    addresses: {
-      "mainnet-alpha"?: string[],
-      "goerli-alpha"?: string[],
-      "goerli-alpha-2"?: string[],
-    },
-    [k: string]: unknown,
-  }[];
-  categories: Category[];
-  [k: string]: unknown;
+    id: string;
+    displayName: string;
+    description: string;
+    host:
+        | string
+        | {
+              "mainnet-alpha"?: string;
+              "goerli-alpha"?: string;
+              "goerli-alpha-2"?: string;
+              others?: string;
+          };
+    contracts: {
+        tag: string;
+        implements?: ("erc721" | "erc20" | "erc1155")[];
+        addresses: {
+            "mainnet-alpha"?: string[];
+            "goerli-alpha"?: string[];
+            "goerli-alpha-2"?: string[];
+        };
+        [k: string]: unknown;
+    }[];
+    categories: Category[];
+    [k: string]: unknown;
 }
 
 export interface Project extends ProjectMetadata {
-  icon: string;
-  cover: string;
+    icon: string;
+    cover: string;
 }
